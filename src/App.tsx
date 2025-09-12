c150b5faafc1e1dc112636a1c880886b77c517e8
Mike <mixakievchanen@gmail.com>
2025-09-12 13:40:05 +0300

    rename branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
       <ModeToggle />
-      <Button>button</Button>
+      <Button>Button</Button>
     </div>
   );
 }
