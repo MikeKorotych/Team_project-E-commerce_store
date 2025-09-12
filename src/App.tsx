7b02dc08ee25802d227bb73059bfbcca1d8213a6
Mike <mixakievchanen@gmail.com>
2025-09-12 16:55:56 +0300

    folders updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import { ModeToggle } from './components/mode-toggle';
 import { Button } from './components/ui/button';
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
 function App() {
   return (
-    <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+    <div className="max-w-[100vw] h-[100vh] flex flex-col gap-4 justify-center items-center">
       <ModeToggle />
-      <Button>Button</Button>
+      <Button className="test">Button</Button>
+
+      <div className="">
+        <Carousel className="w-[600px]">
+          <CarouselContent>
+            <CarouselItem>
+              <img src="https://placehold.co/600x400" alt="logo" />
+            </CarouselItem>
+            <CarouselItem>
+              <img src="https://placehold.co/600x400" alt="logo" />
+            </CarouselItem>
+            <CarouselItem>
+              <img src="https://placehold.co/600x400" alt="logo" />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious className="rounded-[0px] h-full max-md:hidden" />
+          <CarouselNext className="rounded-[0px] h-full" />
+        </Carousel>
+      </div>
     </div>
   );
 }
