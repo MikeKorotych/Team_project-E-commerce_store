40eb9ee204221aa4f515d1ce9d2510b0a30d47d3
Alex Lesnoy <123098237+bebelekele@users.noreply.github.com>
2025-09-15 14:04:25 +0300

    feat(dev-2): create product card and started creating PhonesPage

--- src/features/phones/PhonesPage.tsx
+++ src/features/phones/PhonesPage.tsx
@@ -1,5 +1,6 @@
-import { supabase } from '@/utils/supabase';
-import { useQuery } from '@tanstack/react-query';
+import { ProductCard } from "@/components/ProductCard";
+import { supabase } from "@/utils/supabase";
+import { useQuery } from "@tanstack/react-query";
 
 //todo create type for Product
 
@@ -11,9 +12,9 @@ import { useQuery } from '@tanstack/react-query';
 const fetchPhones = async () => {
   // request to Supabase
   const { data, error } = await supabase
-    .from('products')
-    .select('*')
-    .eq('category', 'phones');
+    .from("products")
+    .select("*")
+    .eq("category", "phones");
 
   // process error
   if (error) {
@@ -32,7 +33,7 @@ const PhonesPage = () => {
     isError, // `true`, якщо функція `fetchPhones` кинула помилку
     error, // Об'єкт помилки
   } = useQuery({
-    queryKey: ['products', 'phones'], // 1. Унікальний ключ для цього запиту
+    queryKey: ["products", "phones"], // 1. Унікальний ключ для цього запиту
     queryFn: fetchPhones, // 2. Функція, яка буде виконувати запит
   });
 
@@ -49,16 +50,32 @@ const PhonesPage = () => {
   }
 
   return (
-    <div>
-      PhonesPage
-      <ul>
-        {phones?.map((phone) => (
-          <li key={phone.id}>
-            {phone.name}
-            <img src={phone.images[0]} alt="" />
-          </li>
-        ))}
-      </ul>
+    <div className="grid grid-cols-24 gap-[8px]">
+      <h1 className="text-5xl/[56px] font-bold col-start-1 col-end-9 mt-[40px]">
+        Mobile phones
+      </h1>
+      <span className="text-sm col-start-1 col-end-3 text-[#75767F]">
+        {phones?.length} models
+      </span>
+      <div className="flex flex-row gap-2 col-start-1 col-end-7 mt-[32px]">
+        <div className="flex flex-1 flex-col gap-2">
+          <span className="text-sm text-[#75767F]">Sort by:</span>
+          <select className="bg-[#323542] px-3 py-2 text-sm focus:border border-[#905BFF]">
+            <option>Newest</option>
+          </select>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="text-sm text-[#75767F]">Items on page</span>
+          <select className="bg-[#323542] px-3 py-2 text-sm">
+            <option className="bg-transparent">16</option>
+          </select>
+        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-8 col-start-1 col-end-25 mt-[16px] mb-[40px]">
+        {phones?.map((phone) => {
+          return <ProductCard key={phone.id} product={phone} />;
+        })}
+      </div>
     </div>
   );
 };
