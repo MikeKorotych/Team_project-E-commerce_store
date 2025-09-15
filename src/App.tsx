168e0c8e7bc4ec2f98c08fd980fe1174a46054cc
Mike <mixakievchanen@gmail.com>
2025-09-15 18:05:53 +0300

    header adaptation done

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       <Header />
 
-      <div className="max-w-[1200px] px-[32px] mx-auto">
+      <div className="mx-auto max-w-[1200px] min-[1200px]:px-[32px] min-[1200px]:py-[56px] px-4 py-8 sm:px-8 sm:py-10">
         <Outlet />
       </div>
     </div>
