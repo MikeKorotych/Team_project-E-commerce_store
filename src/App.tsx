c28f887a4ce287b65344cf151dfd8431b64467d1
chips <sanchelo0822@gmail.com>
2025-09-15 21:24:06 +0300

    dev fotter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router';
 import { Header } from './components/Header';
+import Footer from './components/Fotter';
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
 
-      <div className="max-w-[1200px] px-[32px] mx-auto">
+      <div className="flex-grow max-w-[1200px] px-[32px] mx-auto">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
