6821745ef5e27525a4c767b111323cd5d98ad7b6
Mike <mixakievchanen@gmail.com>
2025-09-15 11:34:10 +0300

    liks added to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router';
 
 export const Navbar = () => (
   <div className="grid grid-cols-4 gap-4 p-4 border-b mb-4">
-    <Button variant={"ghost"}>
-      HOME
+    <Button variant={'ghost'}>
+      <Link to="/home">HOME</Link>
     </Button>
-    <Button variant={"ghost"}>
-      PHONES
+    <Button variant={'ghost'}>
+      <Link to="/phones">PHONES</Link>
     </Button>
-    <Button variant={"ghost"}>
-      TABLETS
+    <Button variant={'ghost'}>
+      <Link to="/tables">TABLETS</Link>
     </Button>
-    <Button variant={"ghost"}>
-      ACCESSORIES
+    <Button variant={'ghost'}>
+      <Link to="/accessories">ACCESSORIES</Link>
     </Button>
   </div>
 );
