ba8f7043478bae4b2901f9b81607122b4ea47dd1
Mike <mixakievchanen@gmail.com>
2025-09-18 16:40:24 +0300

    build error fixed

--- src/components/HomeShopCategory.tsx
+++ src/components/HomeShopCategory.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router";
+import React from 'react';
+import { Link } from 'react-router';
 
 type Props = {
   supabaseUrl: string;
@@ -11,40 +11,43 @@ export const HomeShopCategory: React.FC<Props> = ({ supabaseUrl }) => {
       <h2 className="text-3xl font-bold mb-4 py-6">Shop by category</h2>
       <div className="flex gap-[10px] justify-between flex-col sm:flex-row items-center">
         <div>
-          <Link to="/phones">
+          <Link to="/phones" className="overflow-hidden block">
             <img
               src={`${supabaseUrl}/storage/v1/object/public/product-images/img/cat-phones.png`}
               alt="phones-category"
+              className="hover:scale-110 transition-all"
             />
           </Link>
 
           <div className="flex flex-col py-6">
             <span>Mobile phones</span>
-            <span className="text-dark">{`${"length"} models`}</span>
+            <span className="text-dark">{`${'length'} models`}</span>
           </div>
         </div>
         <div>
-          <Link to="/tablets">
+          <Link to="/tablets" className="overflow-hidden block">
             <img
               src={`${supabaseUrl}/storage/v1/object/public/product-images/img/cat-tablets.png`}
               alt="tablets-category"
+              className="hover:scale-110 transition-all"
             />
           </Link>
           <div className="flex flex-col py-6">
             <span>Tablets</span>
-            <span className="text-dark">{`${"length"} models`}</span>
+            <span className="text-dark">{`${'length'} models`}</span>
           </div>
         </div>
         <div>
-          <Link to="/accessories">
+          <Link to="/accessories" className="overflow-hidden block">
             <img
               src={`${supabaseUrl}/storage/v1/object/public/product-images/img/cat-accessories.png`}
               alt="accessories-category"
+              className="hover:scale-110 transition-all"
             />
           </Link>
           <div className="flex flex-col py-6">
             <span className="py-">Accessories</span>
-            <span className="text-dark">{`${"length"} models`}</span>
+            <span className="text-dark">{`${'length'} models`}</span>
           </div>
         </div>
       </div>
