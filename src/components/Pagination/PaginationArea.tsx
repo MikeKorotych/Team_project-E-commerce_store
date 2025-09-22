39f9ebf2a434fa95119cdb3930db60e98c21c97f
Alex Lesnoy <123098237+bebelekele@users.noreply.github.com>
2025-09-22 12:36:38 +0300

    refactor: fix bugs of Pagination

--- src/components/Pagination/PaginationArea.tsx
+++ src/components/Pagination/PaginationArea.tsx
@@ -1,7 +1,7 @@
 import type { Product } from "@/types/Product";
 import { ProductCard } from "../ProductCard";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { PaginationButtons } from "./PaginationButtons";
 
 type Props = {
@@ -11,29 +11,40 @@ type Props = {
 
 export const PaginationArea: React.FC<Props> = ({ products, itemsPerPage }) => {
   const { page } = useParams(); // <<< Получаем текущую страницу из URL
+  const navigate = useNavigate();
 
   const [currentPage, setCurrentPage] = useState(parseInt(page || "1", 10)); // <<< Текущая страница
 
   const totalPages = Math.ceil(products.length / itemsPerPage); // <<< Общее количество страниц
 
-  const shownProducts = products.slice( 
+  const shownProducts = products.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   ); // <<< Продукты для текущей страницы
 
   useEffect(() => {
-    setCurrentPage(parseInt(page || "1", 10));
-  }, [page]); 
+    if (parseInt(page || "1", 10) > totalPages) {
+      setCurrentPage(totalPages);
+      navigate(`../${totalPages}?limit=${itemsPerPage}`)
+    } else {
+      setCurrentPage(parseInt(page || "1", 10));
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, totalPages]);
 
   return (
     <>
       <div className="products-table">
         {shownProducts.map((product) => (
-          <ProductCard key={product.id} product={product} /> 
-        ))} 
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
       <div className="w-auto flex justify-center mx-auto">
-        <PaginationButtons totalPages={totalPages} currentPage={currentPage} itemsPerPage={itemsPerPage} /> {/* <<< Кнопки пагинации */}
+        <PaginationButtons
+          totalPages={totalPages}
+          currentPage={currentPage}
+          itemsPerPage={itemsPerPage}
+        />
       </div>
     </>
   );
